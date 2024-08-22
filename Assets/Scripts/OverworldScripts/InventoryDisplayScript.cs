47fb731a7594af99ad695032d475980a86a3397e
Elusive239 <56003608+Elusive239@users.noreply.github.com>
2024-08-22 02:31:29 -0400

    swapped overworld code from main to use new item system

--- Assets/Scripts/OverworldScripts/InventoryDisplayScript.cs
+++ Assets/Scripts/OverworldScripts/InventoryDisplayScript.cs
@@ -10,14 +10,14 @@ public class InventoryDisplayScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        populateItems();
+        PopulateItems();
     }
 
-    public void populateItems(){
-        foreach(Item i in player.items){
+    public void PopulateItems(){
+        foreach(ItemSO i in player.items){
             GameObject option;
             option = Instantiate(itemOptionPrefab, itemPanel.transform, true);
-            option.name = i.name;
+            option.name = i.item.itemName;
         }
     }
 }
--- Assets/Scripts/OverworldScripts/OverworldItemScript.cs
+++ Assets/Scripts/OverworldScripts/OverworldItemScript.cs
@@ -11,17 +11,17 @@ public class OverworldItemScript : MonoBehaviour
     public Button optionButton;
     public TMP_Text optionName;
     public TMP_Text optionVal;
-    Item item;
+    ItemSO currentItem;
     // Start is called before the first frame update
     void Start()
     {
         //Find this option and set our text values accordingly
-        foreach(Item i in player.items){
-            if(i.name == option.name){
-                item = i;
-                optionName.text = item.name;
-                optionVal.text = item.desc + "";
-                if(item.dmgVal > 0){
+        foreach(ItemSO i in player.items){
+            if(i.item.itemName == option.name){
+                currentItem = i;
+                optionName.text = currentItem.item.itemName;
+                optionVal.text = currentItem.item.itemDesc + "";
+                if(!currentItem.item.CheckFlag(ItemFlag.CONSUMABLE)){
                     optionButton.interactable = false;
                 }
             }
@@ -29,24 +29,24 @@ public class OverworldItemScript : MonoBehaviour
     }
     //enemy takes damage based on the selection, then player passes priority
     public void onOptionSelect(){
-        if(player.HP < player.MaxHP){
-            if(item.healVal > 0){
-                player.heal(item.healVal);
-            }
-            if(item.consumable){
-                int count = 0;
-                foreach(Item i in player.items){
-                    if(i.name == item.name){
-                        i.quantity -= 1;
-                    }
-                    if(i.quantity < 1){
-                        List<Item> itemList = new List<Item>(player.items);
-                        itemList.RemoveAt(count);
-                        player.items = itemList.ToArray();
-                        Destroy(option);
-                    }
-                    count++;
+        if(!currentItem.item.CheckFlag(ItemFlag.CONSUMABLE)){
+            return;
+        }
+
+        if(currentItem.item.CheckFlag(ItemFlag.HEALING) && player.maxHealth < player.currentHealth){
+            player.Heal(currentItem.item.itemModValue);
+            int count = 0;
+            foreach(ItemSO i in player.items){
+                if(i.item.itemName == currentItem.item.itemName){
+                    i.item.itemQuantity -= 1;
                 }
+                if(i.item.itemQuantity < 1){
+                    List<ItemSO> itemList = new List<ItemSO>(player.items);
+                    itemList.RemoveAt(count);
+                    player.items = itemList.ToArray();
+                    Destroy(option);
+                }
+                count++;
             }
         }
     }
